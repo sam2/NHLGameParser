c2e774707439ded9c16addb37251247deb676e39
sam2 <samhaliburton@gmail.com>
2016-09-24 14:35:58 -0300

    Initial commit

---
+++ NHLGameParser/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using NHLStatsModel;
+
+namespace NHLDatabaseController
+{
+    class Program
+    {
+        private const string k_DatabaseName = "test";
+        private const string k_CollectionName = "testViews";
+
+        static void Main(string[] args)
+        {
+            var client = new MongoClient();
+            var db = client.GetDatabase(k_DatabaseName);
+            var views = db.GetCollection<NHLGame>(k_CollectionName);
+
+            List<NHLGame> allgames = new List<NHLGame>();
+            
+            for (int i= 1; i <= 30; i ++)
+            {
+                DateTime date = new DateTime(2016, 04, i);                             
+                List<NHLGame> games = NHLGameParser.GetNHLGames(date);
+                allgames.AddRange(games);                
+            }          
+
+            Console.ReadLine();
+        }
+    }
+}
